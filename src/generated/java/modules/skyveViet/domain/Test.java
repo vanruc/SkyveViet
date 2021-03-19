89cc168f2d4b4f31e1f837968fc7e8235bade96f
DannyChu <rucvan.pr@gmail.com>
2021-03-19 23:34:18 +0000

    Initial Project

---
+++ src/generated/java/modules/skyveViet/domain/Test.java
@@ -0,0 +1,96 @@
+package modules.skyveViet.domain;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import org.skyve.CORE;
+import org.skyve.domain.messages.DomainException;
+import org.skyve.impl.domain.AbstractPersistentBean;
+
+/**
+ * Test
+ * 
+ * @stereotype "persistent"
+ */
+@XmlType
+@XmlRootElement
+public class Test extends AbstractPersistentBean {
+	/**
+	 * For Serialization
+	 * @hidden
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/** @hidden */
+	public static final String MODULE_NAME = "skyveViet";
+	/** @hidden */
+	public static final String DOCUMENT_NAME = "Test";
+
+	/** @hidden */
+	public static final String namePropertyName = "name";
+
+	/**
+	 * Name
+	 **/
+	private String name;
+
+	@Override
+	@XmlTransient
+	public String getBizModule() {
+		return Test.MODULE_NAME;
+	}
+
+	@Override
+	@XmlTransient
+	public String getBizDocument() {
+		return Test.DOCUMENT_NAME;
+	}
+
+	public static Test newInstance() {
+		try {
+			return CORE.getUser().getCustomer().getModule(MODULE_NAME).getDocument(CORE.getUser().getCustomer(), DOCUMENT_NAME).newInstance(CORE.getUser());
+		}
+		catch (RuntimeException e) {
+			throw e;
+		}
+		catch (Exception e) {
+			throw new DomainException(e);
+		}
+	}
+
+	@Override
+	@XmlTransient
+	public String getBizKey() {
+		try {
+			return org.skyve.util.Binder.formatMessage("Test - {name}", this);
+		}
+		catch (@SuppressWarnings("unused") Exception e) {
+			return "Unknown";
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return ((o instanceof Test) && 
+					this.getBizId().equals(((Test) o).getBizId()));
+	}
+
+	/**
+	 * {@link #name} accessor.
+	 * @return	The value.
+	 **/
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * {@link #name} mutator.
+	 * @param name	The new value.
+	 **/
+	@XmlElement
+	public void setName(String name) {
+		preset(namePropertyName, name);
+		this.name = name;
+	}
+}
